f04949844323085af9163ccb1349557d42ccf691
andrewbastian <andrew.bastian@gmail.com>
2020-09-04 10:10:37 -0700

    ex3 passing tests

--- hashtables/ex3/ex3.py
+++ hashtables/ex3/ex3.py
@@ -3,15 +3,24 @@ def intersection(arrays):
     YOUR CODE HERE
     """
     # Your code here
+    dictionary = {}
 
-    return result
+    for index in arrays:
+        for arr in index:
+            if arr in dictionary:
+                dictionary[arr] += 1
+            else:
+                dictionary[arr] = 1
 
+    res = [arr[0] for arr in dictionary.items() if arr[1] == len(arrays)]
+
+    return res
 
 if __name__ == "__main__":
     arrays = []
 
-    arrays.append(list(range(1000000, 2000000)) + [1, 2, 3])
-    arrays.append(list(range(2000000, 3000000)) + [1, 2, 3])
-    arrays.append(list(range(3000000, 4000000)) + [1, 2, 3])
+    arrays.append(list(range(10, 20)) + [1, 2, 3])
+    arrays.append(list(range(20, 30)) + [1, 2, 3])
+    arrays.append(list(range(30, 40)) + [1, 2, 3])
 
     print(intersection(arrays))
